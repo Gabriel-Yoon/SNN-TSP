c354355f021156e2babc302733eb6ca50c42db98
Gabriel Yoon <a9oldbird@gmail.com>
2021-04-07 14:22:22 +0900

    added time printer and progress

--- core.cpp
+++ core.cpp
@@ -9,15 +9,19 @@
 
 #define PBSTR "||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||"
 #define PBWIDTH 100
+extern int INIT_VAL = 0;
 
 using namespace std;
 
 void printProgress(double percentage) {
     int val = (int)(percentage * 100);
-    int lpad = (int)(percentage * PBWIDTH);
-    int rpad = PBWIDTH - lpad;
-    printf("\r%3d%% [%.*s%*s]", val, lpad, PBSTR, rpad, "");
-    fflush(stdout);
+    if (val != INIT_VAL) {
+        INIT_VAL++;
+        int lpad = (int)(percentage * PBWIDTH);
+        int rpad = PBWIDTH - lpad;
+        printf("\r%3d%% [%.*s%*s]", val, lpad, PBSTR, rpad, "");
+        fflush(stdout);
+    }
 }
 
 
--- tester.cpp
+++ tester.cpp
@@ -6,6 +6,7 @@
 #include <queue>
 #include <stdio.h>
 #include <stdlib.h>  
+#include <time.h>
 //#include <windows.h> // Sleep ÇÔ¼ö
 
 #include "core.hpp"
@@ -24,6 +25,11 @@ struct cmp {
 };
 
 int main(void) {
+    clock_t start, end;
+    double result;
+
+    start = clock();
+    
     system("color 0A");
     core mycore;
     mycore.initialize();
@@ -40,5 +46,13 @@ int main(void) {
     mycore.weight_setup(same_WTA_diff_cities, adj_WTA_same_cities, non_adj_WTA_same_cities);
     */
 
+    // Time print
+    end = clock();
+    result = (double)(end - start);
+    cout << "result : " << ((result) / CLOCKS_PER_SEC) << " seconds" << endl;
+    printf("%f", result / CLOCKS_PER_SEC);
+
     return 0;
+
+
 }
