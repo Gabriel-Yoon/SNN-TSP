659d26b75a3f0bb7aab43c1364eb2d51c0692cd8
Gabriel Yoon <53244335+Gabriel-Yoon@users.noreply.github.com>
2021-04-29 14:54:44 +0900

    added rusage and int arg

--- tester.cpp
+++ tester.cpp
@@ -7,6 +7,9 @@
 #include <stdio.h>
 #include <stdlib.h>  
 #include <time.h>
+#include <sys/time.h>
+#include <sys/resource.h>
+
 //#include <windows.h> // Sleep ÇÔ¼ö
 
 #include "core.hpp"
@@ -24,7 +27,7 @@ struct cmp {
     }
 };
 
-int main(void) {
+int main(int argc, char *argv[]) {
     clock_t start, end;
     double result;
 
@@ -46,13 +49,17 @@ int main(void) {
     mycore.weight_setup(same_WTA_diff_cities, adj_WTA_same_cities, non_adj_WTA_same_cities);
     */
 
+    struct rusage usage;
+    getrusage(RUSAGE_SELF, &usage);
+    cout << "User time " << usage.ru_utime.tv_sec + usage.ru_utime.tv_usec * 10e-7 << "sec" << endl;
+    cout << "System time" << usage.ru_stime.tv_sec + usage.ru_utime.tv_usec * 10e-7 << "sec" << endl;
+    cout << "maxurss " << usage.ru_maxrss / 1024 << " MB" << endl;
+
     // Time print
     end = clock();
     result = (double)(end - start);
     cout << "result : " << ((result) / CLOCKS_PER_SEC) << " seconds" << endl;
     printf("%f", result / CLOCKS_PER_SEC);
-
-    return 0;
 
 
 }
