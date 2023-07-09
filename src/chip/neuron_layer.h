b3ebba55ea05a451f9530cdeef007bbf2f4fbf91
Gabriel <temp@Gabriel-MacBook-Pro.local>
2023-07-09 13:39:39 +0900

    feat: Add neuron_layer template class for each visible and hidden layer

---
+++ src/chip/neuron_layer.h
@@ -0,0 +1,61 @@
+#ifndef _NEURON_LAYER_H_
+#define _NEURON_LAYER_H_
+
+#include <vector>
+#include <type_traits>
+#include "neuron.h"
+
+// SFINAE
+// HOW TO USE neuron_layer<T>
+// neuron_layer<T> is constructed only when the T is inherited or from the neuron class.
+// neuron_layer<neuron> arr1;       -> ok
+// neuron_layer<lif_neuron> arr2;   -> ok
+// neuron_layer<int> arr3;          -> compile error
+
+//**************************************************************************************************************//
+template <typename NeuronType, std::enable_if_t<std::is_base_of<neuron, NeuronType>::value, bool> = true>
+class neuron_layer
+{
+    /*---------------------fields-----------------*/
+    public: std::vector<NeuronType> _neurons;
+    //private: int maxSize;
+    /*---------------------methods----------------*/
+    // Constructor
+    public: neuron_layer(){}
+    // Destructor
+    public: ~neuron_layer();
+    //----------------------------------------------------
+    public: void ManualSet(param& _params){
+        for (auto _neuron : this->_neurons){
+            _neuron.ManualSet(_params);
+        }
+    }
+    //----------------------------------------------------
+    public: void addElement(const neuron& _neuron){
+        if (this->_neurons.size()) {
+            this->_neurons.push_back(_neuron);
+        }
+    }
+    //----------------------------------------------------
+	public: void RandomWalk(){
+        for (auto _neuron : this->_neurons){
+            _neuron.memV_RandomWalk();
+        }
+    }
+    //----------------------------------------------------
+	// template <typename U, std::enable_if_t<std::is_same<NeuronType, lif_neuron>::value, bool> == true>
+    // public: Leak(double& tpre, double& tnow){
+    //     for (auto _neuron : this->elements){
+    //         _neuron.memV_Leak(tpre, tnow);
+    //     }
+    // }
+    //----------------------------------------------------
+	public: void CompareThreshold(){
+
+    }
+
+};
+
+
+
+#endif
