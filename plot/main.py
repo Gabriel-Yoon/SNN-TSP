f95c869ad0e05f61ba1573cd8b1cb4320abf78d2
Gabriel <temp@Gabriel-MacBook-Pro.local>
2023-07-18 14:44:34 +0900

    feat: Add plotting utility - synapse weight plot for t=0 is automatically saved after run

--- plot/main.py
+++ plot/main.py
@@ -1,8 +1,8 @@
 from plotFunctions import plotSynapseWeightsFromJson, plotSpikeFromJson, plotEventPlotFromJson, plot_spike_raster
 
 
-filename = "../weight_gp_gm_8560.json"
-plotSynapseWeightsFromJson(filename, "8560.png")
+filename = "weight_gp_gm_0.json"
+plotSynapseWeightsFromJson(filename, "0.png")
 #tsp_data_filename = "/Users/gabriel/Development/SNN-TSP/build/tsp_data.json"
 #spike_history_filename = "/Users/gabriel/Development/SNN-TSP/build/spike_history.json"
 #plotEventPlotFromJson(tsp_data_filename, spike_history_filename)
