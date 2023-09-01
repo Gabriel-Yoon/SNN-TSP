f675397a903466eed8fff7430ab4a329a6151d3a
Gabriel-Yoon <a9oldbird@gmail.com>
2023-09-01 10:02:52 +0900

    feat: Add distance matrix data to core, change of code style

--- src/chip/core.h
+++ src/chip/core.h
@@ -41,93 +41,94 @@
 // Forward declarations
 class tsp;
 
-
 class core
 {
-	private: param params;
-	private: rng _rng;
+private:
+	param params;
+	rng _rng;
 	std::ofstream os;
 	/*---------------------fields-----------------*/
-	typedef std::priority_queue<std::pair<double, spike*>, std::vector<std::pair<double, spike*>>, spk_cmp> SpikeMagazine;
-	typedef std::priority_queue<std::pair<double, random_walk*>, std::vector<std::pair<double, random_walk*>>, random_walk_cmp> RandomWalkSchedule;
+	typedef std::priority_queue<std::pair<double, spike *>, std::vector<std::pair<double, spike *>>, spk_cmp> SpikeMagazine;
+	typedef std::priority_queue<std::pair<double, random_walk *>, std::vector<std::pair<double, random_walk *>>, random_walk_cmp> RandomWalkSchedule;
 	typedef neuron_layer<lif_neuron> LIFNeuronLayer;
 	typedef synapse_array<synapse> PCMSynapseArray;
 	typedef std::vector<std::pair<double, int>> ShellCatcher;
 
+public:
+	int _numCity;
+	int _solutionDistance;
+	std::vector<int> _optimalItinerary;
+	std::vector<int> _optimalItineraryNeuronNum;
+	std::vector<std::vector<double>> _distanceMatrix;
 
-	public: int _numCity;
-	public: int _solutionDistance;
-	public: std::vector<int> _optimalItinerary;
-	public: std::vector<int> _optimalItineraryNeuronNum;
+private:
+	int num_neurons[2];
+	int num_neurons_principal[2];
+	int num_neurons_bias[2];
+	LIFNeuronLayer visibleLayer;
+	LIFNeuronLayer hiddenLayer;
+	PCMSynapseArray synapseArray;
+	SpikeMagazine visibleMagazine;
+	SpikeMagazine hiddenMagazine;
+	RandomWalkSchedule visibleRandomWalkSchedule;
+	RandomWalkSchedule hiddenRandomWalkSchedule;
+	ShellCatcher visibleShellCatcher; // <time, neuron_index>
+	ShellCatcher hiddenShellCatcher;  // <time, neuron_index>
 
-	private: int num_neurons[2];
-	private: int num_neurons_principal[2];
-	private: int num_neurons_bias[2];
-
-	private: LIFNeuronLayer visibleLayer;
-	private: LIFNeuronLayer hiddenLayer;
-	private: PCMSynapseArray synapseArray;
-
-	private: SpikeMagazine visibleMagazine;
-	private: SpikeMagazine hiddenMagazine;
-	
-	private: RandomWalkSchedule visibleRandomWalkSchedule;
-	private: RandomWalkSchedule hiddenRandomWalkSchedule;
-
-	private: ShellCatcher visibleShellCatcher; // <time, neuron_index>
-	private: ShellCatcher hiddenShellCatcher; // <time, neuron_index>
-
-	//private: SpikeMagazine ShellCatcher; // this is for recording the spike history
-	private: ShellCatcher spikeRecorder; // <side, neuron_index>
-	private: std::string potentialFilePath[2];
+	// private: SpikeMagazine ShellCatcher; // this is for recording the spike history
+	ShellCatcher spikeRecorder; // <side, neuron_index>
+	std::string potentialFilePath[2];
 
 	/*
 	bool export_spk_en;
-    bool export_ptn_en;
-    int export_spk_param[3];
-    char export_ptn_param[8];
-    std::vector<short> export_ptn_neurons_V;
-    std::vector<short> export_ptn_neurons_h;
-    std::string export_spk_file[8];
-    std::string export_ptn_file[2];
-    ofstream *ofs_spk_int;
-    ofstream *ofs_spk_ext;
-    std::string *export_file;
-    ofstream *ofs_ptn_v;
-    ofstream *ofs_ptn_h;
+	bool export_ptn_en;
+	int export_spk_param[3];
+	char export_ptn_param[8];
+	std::vector<short> export_ptn_neurons_V;
+	std::vector<short> export_ptn_neurons_h;
+	std::string export_spk_file[8];
+	std::string export_ptn_file[2];
+	ofstream *ofs_spk_int;
+	ofstream *ofs_spk_ext;
+	std::string *export_file;
+	ofstream *ofs_ptn_v;
+	ofstream *ofs_ptn_h;
 	*/
 	/*---------------------methods----------------*/
 	// core constructor
-	public: core(const char* param_file, const std::string& tsp_data_file_path);
-	public: void initialize();
-	public: void print_params();
-	public: void run_simulation();
+public:
+	core(const char *param_file, const std::string &tsp_data_file_path);
+	void initialize();
+	void print_params();
+	void run_simulation();
 	// public: void initialize_export();
 
-	private: std::tuple<np::array_2d<uint8_t>, np::array_2d<int8_t>> load_mnist_28(std::string dataset, np::array_1d<int> digits);
+private:
+	std::tuple<np::array_2d<uint8_t>, np::array_2d<int8_t>> load_mnist_28(std::string dataset, np::array_1d<int> digits);
 
-	private: void generateMagazine(double tend);
-	private: void loadMagazine(const char* mag_file);			// load spikes into _visibleMagazine
-	private: void setRandomWalkSchedule(double tend, int side);	// set random walk schedule. use annealing schedule here
+	void generateMagazine(double tend);
+	void loadMagazine(const char *mag_file);		   // load spikes into _visibleMagazine
+	void setRandomWalkSchedule(double tend, int side); // set random walk schedule. use annealing schedule here
 
-	private: int assignTask(spike **run_spike, double& tpre, double& tnow, double& tend, int* magazine_side);	// reload spike from either the visible or hidden magazine
-	private: void shootSpike(spike& run_spike, int& phase); // shoot at other side magazine, create reset, st, dummy event at self magazine
-	private: void reloadSpike(double tnow); // compare
-	private: void eraseTask(int& task_id);
-	
+	int assignTask(spike **run_spike, double &tpre, double &tnow, double &tend, int *magazine_side); // reload spike from either the visible or hidden magazine
+	void shootSpike(spike &run_spike, int &phase);													 // shoot at other side magazine, create reset, st, dummy event at self magazine
+	void reloadSpike(double tnow);																	 // compare
+	void eraseTask(int &task_id);
+
 	// Export Data
-	private: void makeSpikeFile(std::string filename);
-	private: void writeSpikeIntoFile(spike& run_spike);
-	private: void exportSpikeRecorder();
-	private: void exportSpikeHistoryToJson(const std::string& filename);
-	private: void exportNeuronPotentialToJson(double& tnow);
-	private: void exportSynapseWeightsToJson(const std::string& filename, double tnow);
+	void makeSpikeFile(std::string filename);
+	void writeSpikeIntoFile(spike &run_spike);
+	void exportSpikeRecorder();
+	void exportSpikeHistoryToJson(const std::string &filename);
+	void exportNeuronPotentialToJson(double &tnow);
+	void exportSynapseWeightsToJson(const std::string &filename, double tnow);
+	void exportPerformanceMostRecentSpikesToJson(const std::string &filename, double deltaTime, double tend);
 
 	// Potential Update
-	private: void potentialUpdate(spike& run_spike);
+	void potentialUpdate(spike &run_spike);
 
 	// Learning Method
-	private: void STDP(spike& run_spike, int& phase);
+	void STDP(spike &run_spike, int &phase);
 };
 
 #endif
